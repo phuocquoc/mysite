57de09960ef6d0d8888c3a984b33c260718e41e9
Nguyễn Phước Quốc <102180180@sv1.dut.udn.vn>
2025-06-28 22:36:13 +0700

    a8

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from django.urls import path
+from django.http import HttpResponse
+
+# Tạo một view đơn giản cho trang chủ
+def home(request):
+    return HttpResponse("Welcome to the Django app on Heroku!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),  # Đường dẫn cho trang chủ
 ]
