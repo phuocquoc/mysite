edecb79cda71f62e11d4143fc932fda3578b9307
Nguyễn Phước Quốc <102180180@sv1.dut.udn.vn>
2025-06-28 22:25:02 +0700

    a5

--- myproject/settings.py
+++ myproject/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'your_secret_key_here'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
