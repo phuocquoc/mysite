c674440e0ec2bf1755cf1d39bc78fc8da7d771a1
Nguyễn Phước Quốc <102180180@sv1.dut.udn.vn>
2025-06-28 23:33:50 +0700

    b1

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,6 +10,10 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from bot import run_bot
+import threading 
+
+threading.Thread(target=run_bot).start()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
