207fd4f921ee2928b33f1f23c12b9ba471069920
Nguyễn Phước Quốc <102180180@sv1.dut.udn.vn>
2025-06-28 20:49:20 +0700

    a

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -8,7 +8,10 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgres://localhost')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 STATIC_URL = '/static/'
