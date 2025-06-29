a577c651dc69c96e4aaa7cfcdaf6ff4e8992ae9a
Nguyễn Phước Quốc <102180180@sv1.dut.udn.vn>
2025-06-29 10:25:18 +0700

    start project

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(c8fasy1=_wgch40013mzhx!^0=)e-z&y8qbfa9iva82q3&fo_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
